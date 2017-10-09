3952ae3ac9e7df66614c4fd1f3fed847701800e4
allmightyspiff <chrisagallo@gmail.com>
2017-10-09 17:53:33 -0500

    updated intervew thing

---
+++ CS/Day1/interview-math.py
@@ -0,0 +1,22 @@
+"""
+@package examples 
+@author Christopher Gallo
+Bubble Sorting
+"""
+
+class sigma_finder():
+    def sum_divisible_by(self, number, max):
+        p = int(max / number)
+        sum = number * (p * (p + 1)) / 2
+        return int(sum)
+
+    def main(self, limit):
+        limit = limit - 1 
+        result = self.sum_divisible_by(3, limit) + self.sum_divisible_by(5, limit) - self.sum_divisible_by(15, limit)
+        return result
+
+if __name__ == "__main__":
+    limit = 50000000
+    main = sigma_finder()
+    result = main.main(limit)
+    print("The sum of all the multiples of 3 or 5 below %s is %s" % (limit,result))
