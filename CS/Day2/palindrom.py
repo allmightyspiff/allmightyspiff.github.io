7b46adf1ce3931f687de7598757de43d7db30120
Christopher Gallo <chrisagallo@gmail.com>
2017-10-11 22:44:11 -0500

    palindrome examples

---
+++ CS/Day2/palindrom.py
@@ -0,0 +1,18 @@
+"""
+@package palindrome
+@author Christopher Gallo
+palindrome
+"""
+from pprint import pprint as pp
+
+class palindrome():
+
+    def sort(self, my_array):
+        pass
+
+if __name__ == "__main__":
+    string1 = 'Aibohphobia'
+    stromg2 = 'Madam, I’m Adam.'
+    main = palindrome()
+    main.sort(my_array)
+    print(my_array)
--- CS/Day2/palindrom_skeleton.py
+++ CS/Day2/palindrom_skeleton.py
@@ -4,14 +4,42 @@
 palindrome
 """
 from pprint import pprint as pp
+import string
 
 class palindrome():
 
-    def sort(self, my_array):
-        pass
+    def pal_check(self, s):
+        s = self.sanitize_2(s)
+        z = list(s)
+        z.reverse()
+        if list(s) == z:
+            return True
+        
+    def sanitize(self, s):
+        s = s.lower()
+        chars = list(string.ascii_lowercase)
+        clean_s = ''
+        for letter in s:
+            if letter in chars:
+                clean_s = clean_s + letter
+
+        return clean_s
+
+    def sanitize_2(self, s):
+        s = s.lower()
+        good = string.ascii_lowercase
+        bad = string.punctuation + string.whitespace + "'’"
+        xlate = s.maketrans(good, good,bad)
+        clean = s.translate(xlate)
+        return s.translate(xlate)
+
 
 if __name__ == "__main__":
-    my_array = [9, 4, 2, 6, 1, 0, 3, 5]
+    my_string = [
+    	'Aibohphobia',
+    	'Madam, I’m Adam.'
+    ]
     main = palindrome()
-    main.sort(my_array)
-    print(my_array)
+    for s in my_string:
+        if main.pal_check(s):
+            print("%s is a palindrome" % s)
