f465aa3663700fa621401cbef91ecbf0f17f4b4c
Christopher Gallo <chrisagallo@gmail.com>
2017-10-22 22:11:16 -0500

    day5

---
+++ CS/Day5/objGraph_example.py
@@ -0,0 +1,11 @@
+from pprint import pprint as pp
+import time
+import objgraph
+
+if __name__ == "__main__":
+    max_size = 1212345
+    objgraph.show_backrefs(
+        max_size, 
+        filename='sample-backref-graph.png'
+    )
+
