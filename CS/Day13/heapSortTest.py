10aae3d964f42bbd1ad30cde4321d309f52dbb04
Christopher Gallo <chrisagallo@gmail.com>
2017-11-27 23:48:11 -0600

    Day13

--- CS/Day13/heapSortTest.py
+++ CS/Day13/heapSortTest.py
@@ -34,6 +34,46 @@ class heapSort():
             self.heapify(array, i, 0)
         return array
 
+class quickSort():
+
+    def partition(self, arr, low, high):
+        i = ( low - 1 )    # index of smaller element
+        pivot = arr[high]  # pivot
+     
+        for j in range(low , high):
+     
+            # If current element is smaller than or
+            # equal to pivot
+            if   arr[j] <= pivot:
+             
+                # increment index of smaller element
+                i = i+1
+                arr[i], arr[j] = arr[j], arr[i]
+     
+        arr[i+1],arr[high] = arr[high],arr[i+1]
+        return ( i+1 )
+ 
+    # The main function that implements QuickSort
+    # arr[] --> Array to be sorted,
+    # low  --> Starting index,
+    # high  --> Ending index
+     
+    # Function to do Quick sort
+    def sort(self, arr, low = 0, high = None):
+        # Default partition is by highest element
+        if high is None:
+            high = len(arr) - 1
+
+        if low < high:
+     
+            # pi is partitioning index, arr[p] is now
+            # at right place
+            pi = self.partition(arr,low,high)
+     
+            # Separately sort elements before
+            # partition and after partition
+            self.sort(arr, low, pi-1)
+            self.sort(arr, pi+1, high)
 
 
 class TestHeapSort(unittest.TestCase):
@@ -42,6 +82,7 @@ class TestHeapSort(unittest.TestCase):
         self.toSort = [4, 10, 3, 5, 1]
         self.sorted = [1, 3, 4, 5, 10]
         self.heapSort = heapSort()
+        self.quickSort = quickSort()
 
     def test_sorting(self):
         sorted_array = self.heapSort.sort(self.toSort)
@@ -54,6 +95,10 @@ class TestHeapSort(unittest.TestCase):
     def test_empty_array(self):
         sorted_array = self.heapSort.sort([])
         self.assertEqual(sorted_array, [])
+
+    def test_quick_sort(self):
+        sorted_array = self.quickSort.sort(self.toSort)
+        self.assertEqual(self.toSort, self.sorted)
 
 if __name__ == '__main__':
     unittest.main()
