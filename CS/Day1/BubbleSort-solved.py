977f334cef8db81af4f714d838803a42606a7256
allmightyspiff <chrisagallo@gmail.com>
2017-12-12 16:12:45 -0600

    Day1 updates

--- CS/Day1/BubbleSort-solved.py
+++ CS/Day1/BubbleSort-solved.py
@@ -1,39 +1,24 @@
-"""
-@package examples 
-@author Christopher Gallo
-Bubble Sorting
-"""
-from pprint import pprint as pp
-
 class bubbleSort():
 
-    def sort(self, to_sort):
-        """Bubble sorts to_sort
-        """
-        is_sorted = False
-        while not is_sorted:
-            array_len = len(to_sort)
-            jobs_done = True
-            # pp(to_sort)
-            for i in range(0,array_len):
-                # Makes sure we dont go out of bounds
-                if i + 1 == array_len:
+    def sort(self,my_array):
+        array_length = len(my_array)
+        total_number_of_passes = array_length
+        for i in range(0, total_number_of_passes):
+            for j in range(0, array_length):
+                # Avoid going out of array bounds
+                if j == array_length - 1:
                     continue
-                # At least one element was unsorted
-                if to_sort[i] > to_sort[i + 1]:
-                    jobs_done = False
-                    temp = to_sort[i]
-                    to_sort[i] = to_sort[i + 1]
-                    to_sort[i + 1] = temp
-                i = i + 1
-            # If we went over the array without changes, we are done
-            if jobs_done:
-                is_sorted = True
-        return to_sort
+                if my_array[j] > my_array[j + 1]:
+                    temp = my_array[j]
+                    my_array[j] = my_array[j + 1]
+                    my_array[j + 1] = temp
+            # Last element of every pass will be the highest number
+            array_length = array_length - 1
+        return my_array
 
 if __name__ == "__main__":
-    test_array = [5, 9, 4, 1, 2, 3, 8]
+    test_array = [5, 9, 4, 1, 2, 99, 3, 8]
     main = bubbleSort()
     sorted_array = main.sort(test_array)
     print("Jobs Done")
-    pp(sorted_array)
+    print(sorted_array)
---
+++ CS/Day1/yrange.py
@@ -0,0 +1,20 @@
+class yrange:
+    def __init__(self, n):
+        self.i = 0
+        self.n = n
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.i < self.n:
+            i = self.i
+            self.i += 1
+            return i
+        else:
+            raise StopIteration()
+
+if __name__ == "__main__":
+    myrange = yrange(3)
+    for x in myrange:
+        print(x)
